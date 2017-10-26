f069a304c8c54712890ebaf900fe33bc3e4162b9
Keren Arielle Marques Ramos <kmramos@prestadores.amil.com.br>
2017-10-26 18:33:11 -0200

    Adicionando browser sync, renomeando nome do html

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: {
+            baseDir: 'html'
+        }
+    });
+
+    gulp.watch('styles/**/*.scss', ['sass']);
+    gulp.watch("html/*.html").on('change', browserSync.reload);
+});
 
 gulp.task('sass', function () {
     return gulp.src('styles/**/*.scss')
     .pipe(sass({
         style: 'compressed'
     }))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());;
 });
 
-gulp.task('default', function(){
-
-});
+gulp.task('default',['serve']);
